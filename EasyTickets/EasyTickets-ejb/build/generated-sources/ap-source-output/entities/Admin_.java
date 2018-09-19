47495125b0b9d53861eeb8b4f5c67176a0ac1eb5
giatuong <pcituong@gmail.com>
2018-09-20 02:31:30 +0700

    Add cus signup

--- EasyTickets/EasyTickets-ejb/build/generated-sources/ap-source-output/entities/Admin_.java
+++ EasyTickets/EasyTickets-ejb/build/generated-sources/ap-source-output/entities/Admin_.java
@@ -4,7 +4,7 @@ import javax.annotation.Generated;
 import javax.persistence.metamodel.SingularAttribute;
 import javax.persistence.metamodel.StaticMetamodel;
 
-@Generated(value="EclipseLink-2.5.2.v20140319-rNA", date="2018-09-20T02:15:35")
+@Generated(value="EclipseLink-2.5.2.v20140319-rNA", date="2018-09-20T02:29:27")
 @StaticMetamodel(Admin.class)
 public class Admin_ { 
 

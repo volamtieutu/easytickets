cc71a790bdc1f1f2d9552438f46dab6032ecb8d0
giatuong <pcituong@gmail.com>
2018-09-20 02:20:47 +0700

    change glassfish

--- EasyTickets/EasyTickets-ejb/build/generated-sources/ap-source-output/entities/Theater_.java
+++ EasyTickets/EasyTickets-ejb/build/generated-sources/ap-source-output/entities/Theater_.java
@@ -8,7 +8,7 @@ import javax.persistence.metamodel.CollectionAttribute;
 import javax.persistence.metamodel.SingularAttribute;
 import javax.persistence.metamodel.StaticMetamodel;
 
-@Generated(value="EclipseLink-2.5.2.v20140319-rNA", date="2018-09-18T14:57:56")
+@Generated(value="EclipseLink-2.5.2.v20140319-rNA", date="2018-09-20T02:15:35")
 @StaticMetamodel(Theater.class)
 public class Theater_ { 
 

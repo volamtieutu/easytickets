9bc0e6d83e57871cf4041c97febcd721686813e8
giatuong <pcituong@gmail.com>
2018-09-20 12:39:58 +0700

    Update

--- EasyTickets/EasyTickets-ejb/build/generated-sources/ap-source-output/entities/Event_.java
+++ EasyTickets/EasyTickets-ejb/build/generated-sources/ap-source-output/entities/Event_.java
@@ -8,7 +8,7 @@ import javax.persistence.metamodel.CollectionAttribute;
 import javax.persistence.metamodel.SingularAttribute;
 import javax.persistence.metamodel.StaticMetamodel;
 
-@Generated(value="EclipseLink-2.5.2.v20140319-rNA", date="2018-09-20T02:29:27")
+@Generated(value="EclipseLink-2.5.2.v20140319-rNA", date="2018-09-20T12:39:08")
 @StaticMetamodel(Event.class)
 public class Event_ { 
 
--- EasyTickets/EasyTickets-ejb/build/generated-sources/ap-source-output/entities/MovieBooking_.java
+++ EasyTickets/EasyTickets-ejb/build/generated-sources/ap-source-output/entities/MovieBooking_.java
@@ -7,7 +7,7 @@ import javax.annotation.Generated;
 import javax.persistence.metamodel.SingularAttribute;
 import javax.persistence.metamodel.StaticMetamodel;
 
-@Generated(value="EclipseLink-2.5.2.v20140319-rNA", date="2018-09-20T02:29:27")
+@Generated(value="EclipseLink-2.5.2.v20140319-rNA", date="2018-09-20T12:39:08")
 @StaticMetamodel(MovieBooking.class)
 public class MovieBooking_ { 
 

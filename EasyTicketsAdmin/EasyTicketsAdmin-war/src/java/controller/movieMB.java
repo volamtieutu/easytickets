36dac5d757ef85402770a6bbba9a4d451eae1514
WinNguyen07 <winhy.nguyen@gmail.com>
2018-09-24 11:04:08 +0700

    project
    backup

--- EasyTicketsAdmin/EasyTicketsAdmin-war/src/java/controller/movieMB.java
+++ EasyTicketsAdmin/EasyTicketsAdmin-war/src/java/controller/movieMB.java
@@ -5,8 +5,11 @@
  */
 package controller;
 
+import java.util.Date;
+import javax.ejb.EJB;
 import javax.inject.Named;
 import javax.enterprise.context.Dependent;
+import models.MovieFacadeLocal;
 
 /**
  *
@@ -16,10 +19,148 @@ import javax.enterprise.context.Dependent;
 @Dependent
 public class movieMB {
 
+    @EJB
+    private MovieFacadeLocal movieFacade;
+
     /**
      * Creates a new instance of movieMB
      */
+    private int movieID;
+    private String movieName;
+    private String genre;
+    private String language;
+    private String rated;
+    private String cast;
+    private String director;
+    private int runTime;
+    private int ticketPrice;
+    private Date beginDate;
+    private Date endDate;
+    private int point;
+    private String description;
+    private String image;
+    private String media;
     public movieMB() {
+    }
+
+    public int getMovieID() {
+        return movieID;
+    }
+
+    public void setMovieID(int movieID) {
+        this.movieID = movieID;
+    }
+
+    public String getMovieName() {
+        return movieName;
+    }
+
+    public void setMovieName(String movieName) {
+        this.movieName = movieName;
+    }
+
+    public String getGenre() {
+        return genre;
+    }
+
+    public void setGenre(String genre) {
+        this.genre = genre;
+    }
+
+    public String getLanguage() {
+        return language;
+    }
+
+    public void setLanguage(String language) {
+        this.language = language;
+    }
+
+    public String getRated() {
+        return rated;
+    }
+
+    public void setRated(String rated) {
+        this.rated = rated;
+    }
+
+    public String getCast() {
+        return cast;
+    }
+
+    public void setCast(String cast) {
+        this.cast = cast;
+    }
+
+    public String getDirector() {
+        return director;
+    }
+
+    public void setDirector(String director) {
+        this.director = director;
+    }
+
+    public int getRunTime() {
+        return runTime;
+    }
+
+    public void setRunTime(int runTime) {
+        this.runTime = runTime;
+    }
+
+    public int getTicketPrice() {
+        return ticketPrice;
+    }
+
+    public void setTicketPrice(int ticketPrice) {
+        this.ticketPrice = ticketPrice;
+    }
+
+    public Date getBeginDate() {
+        return beginDate;
+    }
+
+    public void setBeginDate(Date beginDate) {
+        this.beginDate = beginDate;
+    }
+
+    public Date getEndDate() {
+        return endDate;
+    }
+
+    public void setEndDate(Date endDate) {
+        this.endDate = endDate;
+    }
+
+    public int getPoint() {
+        return point;
+    }
+
+    public void setPoint(int point) {
+        this.point = point;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public String getImage() {
+        return image;
+    }
+
+    public void setImage(String image) {
+        this.image = image;
+    }
+
+    public String getMedia() {
+        return media;
+    }
+
+    public void setMedia(String media) {
+        this.media = media;
     }
     
 }
